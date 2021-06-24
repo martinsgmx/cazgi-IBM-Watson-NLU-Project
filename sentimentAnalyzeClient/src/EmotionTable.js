f32009ae2ded6e94af625b2b7ba9cd34e0a61235
martinsgmx <martindev.mx@ibox.ru>
2021-06-24 16:57:57 -0500

    [+] table mapped and border table update

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -2,21 +2,40 @@ import React from 'react';
 import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
-    render() {
-      return (  
-        <div>
-          {/*You can remove this line and the line below. */}
-          {JSON.stringify(this.props.emotions)}
-          <table className="table table-bordered">
-            <tbody>
-            {
-                //Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
-            }
-            </tbody>
-          </table>
-          </div>
-          );
-        }
-    
+	render() {
+		return (
+			<div>
+				{/*You can remove this line and the line below. */}
+				{/*{ JSON.stringify( this.props.emotions.emotion ) }*/}
+				<table
+					className="table table-bordered"
+					style={
+						{
+							'border':
+								(this.props.emotions.emotion.sadness > this.props.emotions.emotion.joy)
+									?
+									'red 3px solid' : 'green 3px solid'
+						}
+					}
+				>
+					<tbody>
+					{
+						//Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
+						Object.entries(this.props.emotions.emotion).map(function (mapentry) {
+							return (
+								<tr key={mapentry[0]}>
+									<td>{mapentry[0]}</td>
+									<td>{mapentry[1]}</td>
+								</tr>
+							)
+						})
+					}
+					</tbody>
+				</table>
+			</div>
+		);
+	}
+
 }
+
 export default EmotionTable;
